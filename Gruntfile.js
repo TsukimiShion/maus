c7810fb2b8b740782d7976814b16c915d2ddc8fd
Tsukimi Shion <tsukimishion@gmail.com>
2014-10-19 22:52:01 +0900

    Implement the following methods and classes.
    - maus.J
        - maus.J.jq()
        - maus.J.checked()
        - maus.J.disabled()
        - maus.display()
        - maus.display_*()
        - maus.attr(), etc.
    - maus.controls()
    - maus.Form
        - maus.Form.def()
        - maus.Form.setDef()
        - maus.Form.getDef()
        - maus.Form.lon()
        - maus.Form.on_vchange()
        - maus.Form.off_vchange()
        - maus.Form.get()
        - maus.Form.set()
        - maus.Form.clear()
    - maus.Text
    - maus.CheckBox
    - maus.Radio
    - maus.Select
    - maus.BoolCheckBox
    - maus.CheckBox
    - maus.CheckBox
    - maus.form()

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+    "use strict";
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        bower: {
+            install: {
+                options: {
+                    targetDir: "./lib",
+                    layout: "byComponent",
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: true,
+                    cleanBowerDir: true,
+                },
+            },
+        },
+        uglify: {
+            my_target: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: "build/maus.min.map",
+                },
+                files: {
+                    "build/maus.min.js": "src/maus.js",
+                },
+            },
+        },
+        mocha: {
+            test: {
+                src: ["test/index.html"],
+            },
+        },
+        jshint: {
+            all: ["Gruntfile.js", "src/maus.js", "test/test.js"],
+            options: {
+                jshintrc: true,
+            },
+        },
+        yuidoc: {
+            compile: {
+                name: "<%= pkg.name %>",
+                description: "<%= pkg.description %>",
+                version: "<%= pkg.version %>",
+                url: "<%= pkg.homepage %>",
+                options: {
+                    paths: "src",
+                    outdir: "yuidoc"
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-yuidoc");
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-mocha");
+};
--- README.md
+++ README.md
@@ -2,3 +2,18 @@ maus
 ====
 
 The jQuery extension library (not jQuery plugin).
+
+## INSTALL
+```
+$ bower install maus
+```
+
+## Dependency
+- [jQuery](http://jquery.com/)
+- [underscore.js](http://underscorejs.org/)
+
+## API Reference
+If you want to see the reference of the latest version, please see http://TsukimiShion.github.io/maus .
+Or if you want to see the reference of the specifed version, please see ``` http://TsukimiShion.github.io/maus/semver/ ``` .
+For example, if you want to see the reference of the version 1.0.0, please see http://TsukimiShion.github.io/maus/v1.0.0/ .
+This document is made by [YUIDoc](http://yui.github.io/yuidoc/).
