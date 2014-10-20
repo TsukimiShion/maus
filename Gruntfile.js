3fe3203bbaa1c333bcc201d9e42fab8589c51078
Tsukimi Shion <tsukimishion@gmail.com>
2014-10-20 23:36:59 +0900

    1. Extend the maus.controls() method. 2. Remove the maus.form() method. Instead of this method, please use the maus.controls() method.

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
             compile: {
                 name: "<%= pkg.name %>",
                 description: "<%= pkg.description %>",
-                version: "<%= pkg.version %>",
+                version: "2.0",
                 url: "<%= pkg.homepage %>",
                 options: {
                     paths: "src",
