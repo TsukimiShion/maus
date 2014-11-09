7345991f60a63fe9c97aeaec13405a269e06af52
Tsukimi Shion <tsukimishion@gmail.com>
2014-11-09 17:05:12 +0900

    Implement the following properties and methods and classes.
    - maus.Color
    - maus.colors
    - maus.j

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
             compile: {
                 name: "<%= pkg.name %>",
                 description: "<%= pkg.description %>",
-                version: "2.0",
+                version: "2.1",
                 url: "<%= pkg.homepage %>",
                 options: {
                     paths: "src",
