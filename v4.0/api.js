78766c8fe68b8523af4ad00fdef5e0605a8089df
Tsukimi Shion <tsukimishion@gmail.com>
2014-11-14 14:41:28 +0900

    Release the version 4.0 .

---
+++ v4.0/api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "maus",
+        "maus.BoolCheckBox",
+        "maus.CheckBox",
+        "maus.Color",
+        "maus.Email",
+        "maus.File",
+        "maus.Form",
+        "maus.J",
+        "maus.Number",
+        "maus.Radio",
+        "maus.Select",
+        "maus.Text"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
